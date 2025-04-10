88e660a368699d3d5f5939dd79d1fd0a647ec03f
lukas-f00 <lukaskfronzeck@gmail.com>
2025-04-10 11:06:19 +0200

    All clip functions.
    Finished all 7 clip functions.
    Yet without any documentation.
    
    Added function 4-7.
    Added temprary name-headings to the functions
      for better overview.

--- R/clip_hexagon.R
+++ R/clip_hexagon.R
@@ -1,3 +1,5 @@
+
+# function 1
 load_swf_data <- function(path) {
   swf_raster <- raster(path)
   swf_vector <- rasterToPolygons(swf_raster, fun = function(x) x > 0, dissolve = TRUE)
@@ -7,6 +9,7 @@ load_swf_data <- function(path) {
 
 ###########################################################################################
 
+# function 2
 create_hex_grid <- function(swf_sf, cellsize) {
   hex_grid <- st_make_grid(swf_sf, cellsize = cellsize, square = FALSE, what = "polygons")
   hex_grid <- st_sf(geometry = hex_grid)
@@ -18,6 +21,7 @@ create_hex_grid <- function(swf_sf, cellsize) {
 
 ############################################################################################
 
+# function 3
 plot_hex_ids <- function(hex_grid) {
   plot(st_geometry(hex_grid), border = "black", main = "Hexagon IDs")
   text(st_coordinates(st_centroid(hex_grid)),
@@ -26,3 +30,38 @@ plot_hex_ids <- function(hex_grid) {
 }
 
 ###########################################################################################
+
+# function 4
+clip_swf_to_grid <- function(swf_sf, hex_grid) {
+  swf_clipped <- st_join(swf_sf, hex_grid, join = st_intersects)
+  return(swf_clipped)
+}
+
+##########################################################################################
+
+# function 5
+plot_swf_grid <- function(hex_grid, swf_clipped) {
+  plot(st_geometry(hex_grid), border = "grey")
+  plot(st_geometry(swf_clipped), col = "forestgreen", add = TRUE)
+}
+
+##########################################################################################
+
+# function 6
+clip_swf_to_hex <- function(swf_sf, hex_grid, hex_id) {
+  selected_hex <- hex_grid[hex_grid$hex_id == hex_id, ]
+  swf_clipped <- st_intersection(swf_sf, selected_hex)
+  return(swf_clipped)
+}
+
+#########################################################################################
+
+# function 7
+plot_swf_hex <- function(hex_grid, swf_clipped, hex_id) {
+  selected_hex <- hex_grid[hex_grid$hex_id == hex_id, ]
+  plot(st_geometry(selected_hex), border = "black", lwd = 2, main = paste("SWF in Hexagon", selected_hex$hex_id))
+  plot(st_geometry(swf_clipped), col = "forestgreen", add = TRUE)
+}
+
+
+
