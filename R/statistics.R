513f27bfd304603117ae5a25cd2b08d1d3373a28
lukas-f00 <lukaskfronzeck@gmail.com>
2025-04-23 15:01:03 +0200

    Statistics change 1
    function 10:
    - added units to print of hedge area
    - change return of percentage to ratio
    
    function 11:
    - deleted unneccessary if loop
    - adjusted the combining part of bordering hedge patches because of errors with a small temporary buffer
    
    function 12:
    - change return of percentage to ratio

--- R/statistics.R
+++ R/statistics.R
@@ -3,10 +3,11 @@ hedge_area <- function(swf_clipped, hex_grid, hex_id) {
   selected_hex <- hex_grid[hex_grid$hex_id == hex_id, ]
   hedge_area <- sum(st_area(swf_clipped))
   hex_area <- st_area(selected_hex)
-  percentage <- as.numeric(hedge_area / hex_area) * 100
-  print(paste0("Hexagon ", selected_hex$hex_id, " hedge area: ", hedge_area))
+  ratio <- as.numeric(hedge_area / hex_area)
+  percentage <- ratio * 100
+  print(paste0("Hexagon ", selected_hex$hex_id, " hedge area: ", round(hedge_area, 2), " ", units::deparse_unit(hedge_area)))
   print(paste0("Hexagon ", selected_hex$hex_id, " hedge area percentage: ", round(percentage, 2), "%"))
-  return(list(hedge_area = hedge_area, percentage = percentage))
+  return(list(hedge_area = hedge_area, ratio = ratio))
 }
 
 #######################################################################################
@@ -17,11 +18,9 @@ count_hedge_obj <- function(swf_clipped, hex_grid, hex_id) {
   swf_valid <- st_make_valid(swf_clipped)
   hex_valid <- st_make_valid(selected_hex)
   swf_in_hex <- st_intersection(swf_valid, hex_valid)
-  if (nrow(swf_in_hex) == 0) {
-    print(paste0("Hexagon ", hex_id, " contains 0 hedge objects"))
-    return(0)
-  }
-  dissolved <- st_union(swf_in_hex)
+  buffered <- st_buffer(swf_in_hex, 0.01)
+  dissolved <- st_union(buffered)
+  dissolved <- st_buffer(dissolved, -0.01)
   individual_patches <- st_cast(dissolved, "POLYGON")
   patch_count <- length(individual_patches)
   print(paste0("Hexagon ", hex_id, " contains ", patch_count, " distinct hedge objects"))
@@ -34,9 +33,8 @@ count_hedge_obj <- function(swf_clipped, hex_grid, hex_id) {
 hedge_points_percentage <- function(swf_clipped, random_points, hex_grid, hex_id) {
   selected_hex <- hex_grid[hex_grid$hex_id == hex_id, ]
   inside <- st_intersects(random_points, swf_clipped, sparse = FALSE)
-  percentage <- sum(inside) / nrow(random_points) * 100
-  print(paste0("Hexagon ", selected_hex$hex_id, " percentage of points within hedge objects: ", round(percentage, 2), "%"))
-  return(percentage)
+  ratio <- sum(inside) / nrow(random_points)
+  percentage <- ratio * 100
+  print(paste0("Hexagon ", selected_hex$hex_id, " percentage of points within hedge objects: ", round(percentage, 2), " %"))
+  return(ratio)
 }
-
-######################################################################################
