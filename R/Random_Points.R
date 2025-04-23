bd584e95a484f168b5ae6b62d2ac311ac22cc68d
lukas-f00 <lukaskfronzeck@gmail.com>
2025-04-23 14:54:59 +0200

    Plot Heading 2
    Updated the plot heading of function 9

--- R/Random_Points.R
+++ R/Random_Points.R
@@ -10,7 +10,7 @@ random_points <- function(hex_grid, hex_id, n_points) {
 # function 9
 plot_random_points <- function(hex_grid, swf_clipped, hex_id, random_points) {
   selected_hex <- hex_grid[hex_grid$hex_id == hex_id, ]
-  plot(st_geometry(selected_hex), border = "black", lwd = 2, main = paste("Hexagon ", selected_hex$hex_id))
+  plot(st_geometry(selected_hex), border = "black", lwd = 2, main = paste("Hexagon ", selected_hex$hex_id, " - random points over hedge areas"))
   plot(st_geometry(swf_clipped), col = "forestgreen", add = TRUE)
   plot(st_geometry(random_points), col = "red", pch = 16, cex = 0.6, add = TRUE)
 }
