a28119156f9b4b6492db8862505b3951fccd8f38
lukas-f00 <lukaskfronzeck@gmail.com>
2025-04-26 15:33:08 +0200

    Namespacing - Clip_hexagon
    Added namespacing to the functions

--- R/clip_hexagon.R
+++ R/clip_hexagon.R
@@ -11,9 +11,9 @@
 
 load_swf_data <- function(path) {
   # Loading in the raster data and converting it to polygons, then to a sf-object
-  swf_raster <- raster(path)
-  swf_vector <- rasterToPolygons(swf_raster, fun = function(x) x > 0, dissolve = TRUE)
-  swf_sf <- st_as_sf(swf_vector)
+  swf_raster <- raster::raster(path)
+  swf_vector <- raster::rasterToPolygons(swf_raster, fun = function(x) x > 0, dissolve = TRUE)
+  swf_sf <- sf::st_as_sf(swf_vector)
   return(swf_sf)
 }
 
@@ -32,12 +32,12 @@ load_swf_data <- function(path) {
 
 
 create_hex_grid <- function(swf_sf, diameter) {
-  # Creating a hexagon grid with the extent of the SWF data and a given cellsize. Also converting it to a sf-object
-  hex_grid <- st_make_grid(swf_sf, cellsize = diameter, square = FALSE, what = "polygons")
-  hex_grid <- st_sf(geometry = hex_grid)
+  # Creating a hexagon grid with the extent of the SWF data and agiven cellsize. Also converting it to a sf-object
+  hex_grid <- sf::st_make_grid(swf_sf, cellsize = diameter, square = FALSE, what = "polygons")
+  hex_grid <- sf::st_sf(geometry = hex_grid)
 
   # getting the coordinates of the centroids
-  coords <- st_coordinates(st_centroid(hex_grid))
+  coords <- sf::st_coordinates(st_centroid(hex_grid))
   # Assingning a ID to each hexagon and ranking them in consecutive order
   hex_grid$hex_id <- rank(-coords[,2]) * 1e6 + rank(coords[,1])
   hex_grid$hex_id <- rank(hex_grid$hex_id)
@@ -59,8 +59,8 @@ create_hex_grid <- function(swf_sf, diameter) {
 
 plot_hex_ids <- function(hex_grid) {
   # Plotting the hexagons and adding the IDs into them
-  plot(st_geometry(hex_grid), border = "black", main = "Hexagon IDs")
-  text(st_coordinates(st_centroid(hex_grid)),
+  plot(sf::st_geometry(hex_grid), border = "black", main = "Hexagon IDs")
+  text(sf::st_coordinates(sf::st_centroid(hex_grid)),
        labels = hex_grid$hex_id,
        cex = 0.7, col = "blue")
 }
@@ -81,7 +81,7 @@ plot_hex_ids <- function(hex_grid) {
 
 swf_grid <- function(swf_sf, hex_grid) {
   # Clipping the SWF-data with the hexagon grid
-  swf_clipped <- st_join(swf_sf, hex_grid, join = st_intersects)
+  swf_clipped <- sf::st_join(swf_sf, hex_grid, join = sf::st_intersects)
   return(swf_clipped)
 }
 
@@ -101,8 +101,8 @@ swf_grid <- function(swf_sf, hex_grid) {
 
 plot_swf_grid <- function(hex_grid, swf_clipped) {
   # Plotting the hexgon grid with the SWF data on top
-  plot(st_geometry(hex_grid), border = "grey", main = "Hexagon grid over hedge data")
-  plot(st_geometry(swf_clipped), col = "forestgreen", add = TRUE)
+  plot(sf::st_geometry(hex_grid), border = "grey", main = "Hexagon grid over hedge data")
+  plot(sf::st_geometry(swf_clipped), col = "forestgreen", add = TRUE)
 }
 
 ##########################################################################################
@@ -123,7 +123,7 @@ plot_swf_grid <- function(hex_grid, swf_clipped) {
 clip_swf_to_hex <- function(swf_sf, hex_grid, hex_id) {
   # Selecting a hexagon via its ID and clipping the SWF data with it
   selected_hex <- hex_grid[hex_grid$hex_id == hex_id, ]
-  swf_clipped <- st_intersection(swf_sf, selected_hex)
+  swf_clipped <- sf::st_intersection(swf_sf, selected_hex)
   return(swf_clipped)
 }
 
@@ -147,6 +147,6 @@ plot_swf_hex <- function(hex_grid, swf_clipped, hex_id) {
   selected_hex <- hex_grid[hex_grid$hex_id == hex_id, ]
 
   # Plotting the hexagon together with the accordingly clipped SWF data
-  plot(st_geometry(selected_hex), border = "black", lwd = 2, main = paste("Small Woody Features in Hexagon", selected_hex$hex_id))
-  plot(st_geometry(swf_clipped), col = "forestgreen", add = TRUE)
+  plot(sf::st_geometry(selected_hex), border = "black", lwd = 2, main = paste("Small Woody Features in Hexagon", selected_hex$hex_id))
+  plot(sf::st_geometry(swf_clipped), col = "forestgreen", add = TRUE)
 }
