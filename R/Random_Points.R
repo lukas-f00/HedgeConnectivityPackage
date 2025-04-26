6deba6fd296f699d504cd64056be96765d31229c
lukas-f00 <lukaskfronzeck@gmail.com>
2025-04-26 15:34:32 +0200

    Namespacing - Random_Points
    Added namespacing to all functions

--- R/Random_Points.R
+++ R/Random_Points.R
@@ -15,8 +15,8 @@ random_points <- function(hex_grid, hex_id, n_points) {
   # Selecting a specific hexagon
   selected_hex <- hex_grid[hex_grid$hex_id == hex_id, ]
   # Creating n (n_points) random points over it
-  points <- st_sample(selected_hex, size = n_points)
-  return(st_sf(geometry = points))
+  points <- sf::st_sample(selected_hex, size = n_points)
+  return(sf::st_sf(geometry = points))
 }
 
 #########################################################################################
@@ -40,7 +40,7 @@ plot_random_points <- function(hex_grid, swf_clipped, hex_id, random_points) {
   selected_hex <- hex_grid[hex_grid$hex_id == hex_id, ]
 
   # Plotting the selected hexagon with the matching SWF data and points on top
-  plot(st_geometry(selected_hex), border = "black", lwd = 2, main = paste("Hexagon ", selected_hex$hex_id, " - random points over hedge areas"))
-  plot(st_geometry(swf_clipped), col = "forestgreen", add = TRUE)
-  plot(st_geometry(random_points), col = "red", pch = 16, cex = 0.6, add = TRUE)
+  plot(sf::st_geometry(selected_hex), border = "black", lwd = 2, main = paste("Hexagon ", selected_hex$hex_id, " - random points over hedge areas"))
+  plot(sf::st_geometry(swf_clipped), col = "forestgreen", add = TRUE)
+  plot(sf::st_geometry(random_points), col = "red", pch = 16, cex = 0.6, add = TRUE)
 }
